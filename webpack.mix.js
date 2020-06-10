af1de12c4c7a6e4475f2d8754a79723ac8576bb6
Matej Lajh <matej.lajh@gmail.com>
2020-06-10 10:11:57 +0200

    updated npm packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const mix = require('laravel-mix');
 
 mix
-    .js([
-        'src/js/app.js'
-    ], 'dist/js/all.js')
-    .sass('src/scss/app.scss', 'dist/css')
-    .copy('*.html', 'dist')
-    .copyDirectory('img', 'dist/img')
-    .setPublicPath('dist');
+  .js([
+    'src/js/app.js'
+  ], 'dist/js/all.js')
+  .sass('src/scss/app.scss', 'dist/css')
+  .copy('*.html', 'dist')
+  .copyDirectory('img', 'dist/img')
+  .setPublicPath('dist');
